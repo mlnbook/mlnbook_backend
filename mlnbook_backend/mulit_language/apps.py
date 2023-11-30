d280b6ef524f7fafb53ca71797923bdb6e20d652
deling.mdl <lingnck@163.com>
2023-11-30 23:46:09 +0800

    change path

--- mlnbook_backend/mulit_language/apps.py
+++ mlnbook_backend/mulit_language/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class LanguageDevelopConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "mlnbook_backend.language_develop"
+    name = "mlnbook_backend.mulit_language"
