a2a1c9d6ef181aa0bcb52cb5ca6ff58072df8b0c
deling.mdl <lingnck@163.com>
2023-12-03 19:59:19 +0800

    添加用户表

--- mlnbook_backend/pic_book/models.py
+++ mlnbook_backend/pic_book/models.py
@@ -1,78 +1,8 @@
 # coding=utf-8
 from django.db import models
+from django.conf import settings
 
-# 语言使用排名 https://www.ethnologue.com/
-LANGUAGE_CODE_CHOICES = (
-    ("en_US", "英语"),
-    ("zh_CN", "简体中文"),
-    ("fr_FR", "法语"),
-    ("es_ES", "西班牙语"),
-    ("ar_AE", "阿拉伯语"),
-    ("ru_RU", "俄语"),
-    # 以上为联合国6种官方语言
-    ("pt_BR", "葡萄牙语"),
-    ("ja_JP", "日语"),
-    ("de_DE", "德语"),
-    ("ko_KR", "韩语"),
-    ("in_ID", "印尼语"),
-    ("ms_MY", "马来语"),
-    ("th_TH", "泰语"),
-    ("hi_IN", "北印度语"),
-    # 印度有多种语言，暂忽略 ("mr_IN", "印度马拉地语"),
-    ("tr_TR", "土耳其语"),
-    ("it_IT", "意大利语"),
-    ("cs_CZ", "捷克语"),
-    ("da_DK", "丹麦语"),
-    ("nl_NL", "荷兰语"),
-    ("no_NO", "挪威语"),
-    ("pl_PL", "波兰语"),
-    ("ro_RO", "罗马尼亚语"),
-    ("sv_SE", "瑞典语"),
-    ("tl_PH", "他加禄语-菲律宾"),
-)
-
-LANGUAGE_LEVEL = (
-    ("A1", "入门级，可以做简单的互动与交流"),
-    ("A2", "基础级，能理解并使用熟悉的日常表达法，基本词汇以求满足具体的需求"),
-    ("B1", "进阶级，能理解大部分日常生活中常用句子和表达法"),
-    ("B2", "高阶级，能在与以目标语言为母语的对象做互动时保持一定流畅度"),
-    ("C1", "流利运用级，在社交上、学术上及专业的场合中，皆能灵活的有效地运用语言资源"),
-    ("C2", "精通级，能流利、准确、并即兴的表达自己的意见，并且在较为复杂的情况下亦能有效地表达或区别出言外之意"),
-)
-
-PHASE_LEVEL = (
-    ("preschool", "学前班"),  # 体验式教育，游戏和保育为主
-    ("Kindergarten", "幼儿园"),  # 课本教育，小学知识预输入；一般持续1年。
-    ("elementary_school", "小学"),
-    ("middle_school", "中学"),
-    ("high_school", "高中"),
-    ("university", "大学"),
-)
-
-GRADE_LEVEL = (
-    ("preschool", "学前班"),  # 体验式教育，游戏和保育为主
-    ("Kindergarten", "幼儿园"),  # 课本教育，小学知识预输入；一般持续1年。
-    ("Grade1", "小学一年级"),
-    ("Grade2", "小学二年级"),
-    ("Grade3", "小学三年级"),
-    ("Grade4", "小学四年级"),
-    ("Grade5", "小学五年级"),
-    ("Grade6", "小学六年级"),  # 部分地区无六年级
-    ("Grade7", "初中一年级"),
-    ("Grade8", "初中二年级"),
-    ("Grade9", "初中三年级"),
-    ("Grade10", "高中一年级"),
-    ("Grade11", "高中二年级"),
-    ("Grade12", "高中三年级"),
-)
-
-PRESCHOOL_YEARS = (
-    ("3mm-12mm", "3-12months"),
-    ("12mm-18mm", "12-18months"),
-    ("18mm-2yy", "18months-2years"),
-    ("2yy-3yy", "2-3years"),
-    ("3yy-5yy", "3-5years"),
-)
+from mlnbook_backend.utils.global_choices import LANGUAGE_CODE_CHOICES, LANGUAGE_LEVEL, PHASE_LEVEL, GRADE_LEVEL
 
 CHAPTER_TYPE_CHOICES = (
     ("public", "完全公开"),
@@ -89,8 +19,8 @@ class PicBook(models.Model):
     phase = models.CharField("学段", max_length=16, choices=PHASE_LEVEL, default="preschool")
     grade = models.CharField("学段", max_length=16, choices=GRADE_LEVEL, default="preschool")
     preschool_year = models.CharField("学前学龄", max_length=16, blank=True, help_text="学前课程细分，非学前为空值")
-    cover_img = models.ImageField("封面图", max_length=500)
-    author = models.CharField("作者ID", max_length=36)
+    cover_img = models.ImageField("封面图", max_length=500, blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ctime = models.DateTimeField(auto_created=True)
     utime = models.DateTimeField(auto_now=True)
 
@@ -104,7 +34,7 @@ class PicBook(models.Model):
 class BookSeries(models.Model):
     title = models.CharField("标题", max_length=500)
     language = models.CharField("语言", max_length=16, default="en_US", choices=LANGUAGE_CODE_CHOICES)
-    author = models.CharField("作者ID", max_length=36)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     pic_books = models.ManyToManyField(PicBook)
     share_state = models.CharField("公开状态", max_length=16, default="public")
     ctime = models.DateTimeField(auto_created=True)
@@ -122,7 +52,7 @@ class VoiceTemplate(models.Model):
     language = models.CharField()
     speaker = models.CharField()
     tts_model = models.CharField()
-    author = models.CharField("作者ID", max_length=36)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ctime = models.DateTimeField(auto_created=True)
     utime = models.DateTimeField(auto_now=True)
 
@@ -151,7 +81,7 @@ class ChapterTemplate(models.Model):
     text_opacity = models.FloatField("文本透明度", default=1, help_text="opacity为不透度度，1为完全显示，0为完全透明; 0.5为半透明 ")
     # voice
     voice_template = models.ForeignKey(VoiceTemplate, on_delete=models.CASCADE)
-    author = models.CharField("作者ID", max_length=36)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ctime = models.DateTimeField(auto_created=True)
     utime = models.DateTimeField(auto_now=True)
 
@@ -172,7 +102,7 @@ class KnowledgePoint(models.Model):
     preschool_year = models.CharField("学前学龄", max_length=16, blank=True, help_text="学前课程细分，非学前为空值")
     pic_file = models.ImageField("图片", upload_to="/images/keywords", null=True)
     pic_style = models.CharField("图片风格", default="realistic")
-    author = models.CharField("作者ID", max_length=36)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ctime = models.DateTimeField(auto_created=True)
     utime = models.DateTimeField(auto_now=True)
 
@@ -191,16 +121,16 @@ class Paragraph(models.Model):
     para_content = models.TextField("段落内容", help_text="段落内容；一般基于知识点+章节复合生成")
     para_content_uniq = models.CharField("段落内容唯一标识", max_length=64, help_text="content文本MD5加密")
     seq_sort = models.IntegerField("排序", default=1, help_text="章节内容排序")
-    author = models.CharField("作者ID", max_length=36)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ctime = models.DateTimeField(auto_created=True)
     utime = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "mlnbook_pic_book_paragraph"
-        unique_together = ["pic_book", "chapter", "content_uniq"]
+        unique_together = ["pic_book", "chapter", "para_content_uniq"]
 
     def __str__(self):
-        return self.content
+        return self.para_content
 
 
 class VoiceFile(models.Model):
@@ -208,6 +138,7 @@ class VoiceFile(models.Model):
     para_content_uniq = models.CharField("段落内容唯一标识", max_length=64, help_text="content文本MD5加密")
     voice_template = models.ForeignKey(VoiceTemplate, on_delete=models.CASCADE)
     voice_file = models.FileField("语音文件", upload_to="pic_book/voice_file")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ctime = models.DateTimeField(auto_created=True)
     utime = models.DateTimeField(auto_now=True)
 
--- mlnbook_backend/users/models.py
+++ mlnbook_backend/users/models.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+
+from mlnbook_backend.utils.global_choices import LANGUAGE_CODE_CHOICES
 
 
 class User(AbstractUser):
@@ -24,3 +29,32 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class Profile(models.Model):
+    # 与User外键连接
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # 个人信息附加字段
+    nick_name = models.CharField("别名", max_length=50, blank=True)
+    native_language = models.CharField("母语", max_length=16, default='zh_CN', choices=LANGUAGE_CODE_CHOICES)
+    learn_language = models.IntegerField("学习语言", max_length=16, default='en_US', choices=LANGUAGE_CODE_CHOICES)
+    ctime = models.DateTimeField(auto_created=True)
+    utime = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "mlnbook_user_profile"
+
+    def __str__(self):
+        return self.user
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+# 同步更新
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
