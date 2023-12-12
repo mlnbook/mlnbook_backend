c079c650f8fba310ca3db4e256df72c631a1f106
deling.mdl <lingnck@163.com>
2023-12-12 22:21:48 +0800

    modify admin

--- mlnbook_backend/pic_book/admin.py
+++ mlnbook_backend/pic_book/admin.py
@@ -7,28 +7,28 @@ from mlnbook_backend.pic_book.models import PicBook, BookSeries, ChapterTemplate
 
 @admin.register(PicBook)
 class PicBookAdmin(admin.ModelAdmin):
-    list_display = ["title", "description", "language", "language_level", "phase", "grade", "author", "ctime"]
+    list_display = ["title", "description", "language", "language_level", "phase", "grade", "user", "ctime"]
     search_fields = ["title"]
     list_filter = ["language", "language_level", "phase"]
 
 
 @admin.register(BookSeries)
 class BookSeriesAdmin(admin.ModelAdmin):
-    list_display = ["title", "description", "language", "author", "share_state", "ctime"]
+    list_display = ["title", "description", "language", "user", "share_state", "ctime"]
     search_fields = ["title"]
     list_filter = ["share_state", "language"]
 
 
 @admin.register(ChapterTemplate)
 class ChapterTemplateAdmin(admin.ModelAdmin):
-    list_display = ["c_type", "title", "description", "author", "text_template", "ctime"]
+    list_display = ["c_type", "title", "description", "user", "text_template", "ctime"]
     search_fields = ["title"]
     list_filter = ["c_type", ]
 
 
 @admin.register(KnowledgePoint)
 class KnowledgePointAdmin(admin.ModelAdmin):
-    list_display = ["knowledge_uniq", "knowledge", "language", "language_level", "phase", "grade", "author", "ctime"]
+    list_display = ["knowledge_uniq", "knowledge", "language", "language_level", "phase", "grade", "user", "ctime"]
     search_fields = ["knowledge"]
     list_filter = ["language", "language_level", "phase"]
 
@@ -36,21 +36,21 @@ class KnowledgePointAdmin(admin.ModelAdmin):
 @admin.register(Paragraph)
 class ParagraphAdmin(admin.ModelAdmin):
     list_display = ["para_content_uniq", "pic_book", "chapter", "knowledge_point", "para_content",
-                    "page_num", "page_para_seq", "author", "ctime"]
+                    "page_num", "page_para_seq", "user", "ctime"]
     search_fields = ["para_content"]
 
 
 @admin.register(ParagraphVoiceFile)
 class ParagraphVoiceFileAdmin(admin.ModelAdmin):
     list_display = ["id", "para_content_uniq", "voice_template", "voice_file", "duration",
-                    "author", "ctime", "utime"]
+                    "user", "ctime", "utime"]
     search_fields = ["para_content_uniq"]
 
 
 @admin.register(KnowledgeVoiceFile)
 class KnowledgeVoiceFileAdmin(admin.ModelAdmin):
     list_display = ["id", "knowledge_uniq", "voice_template", "voice_file", "duration",
-                    "author", "ctime", "utime"]
+                    "user", "ctime", "utime"]
     search_fields = ["knowledge_uniq"]
 
 
@@ -63,5 +63,5 @@ class IllustrationFileAdmin(admin.ModelAdmin):
 @admin.register(VoiceTemplate)
 class VoiceTemplateAdmin(admin.ModelAdmin):
     list_display = ["id", "title", "language", "speaker", "tts_model",
-                    "author", "ctime", "utime"]
+                    "user", "ctime", "utime"]
     search_fields = ["title"]
