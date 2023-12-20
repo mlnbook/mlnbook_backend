71e2a4870df8710ad8f834a08ed174ca12a9369f
Devin Mah <lingnck@163.com>
2023-12-20 18:05:47 +0800

    Update models.py

--- mlnbook_backend/pic_book/models.py
+++ mlnbook_backend/pic_book/models.py
@@ -148,7 +148,7 @@ class LayoutTemplate(models.Model):
     font_color = models.CharField("颜色", max_length=16, default="#0000E0")
     font_family = models.CharField("字体", max_length=50, default="Arial")
     font_size = models.SmallIntegerField("文字大小", default="14", help_text="14px")
-    background_img = models.ImageField("背景图面", upload_to="pic_books/backgroup/", null=True, blank=True)
+    background_img = models.ImageField("背景图面", upload_to="pic_books/background_img/", null=True, blank=True)
     background_color = models.CharField("背景颜色", max_length=16, default="#FFFFFF")
     text_flex_justify = models.CharField("文本主轴位置", max_length=20, default="flex-end", choices=FLEX_JUSTIFY_OPTIONS,
                                          help_text="Flex弹性布局, 设置元素在主轴方向上的对齐方式")
