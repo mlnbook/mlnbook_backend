0697988ed7c186b5ab81aff69d65d88b2a99e404
Devin Mah <lingnck@163.com>
2024-01-02 12:52:26 +0800

    Update views.py

--- mlnbook_backend/pic_book/views.py
+++ mlnbook_backend/pic_book/views.py
@@ -2,9 +2,9 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.parsers import FileUploadParser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django_filters.rest_framework import DjangoFilterBackend
 
 from mlnbook_backend.pic_book.models import PicBook, KnowledgePoint, Chapter, Paragraph, \
     BookSeries, IllustrationFile, LayoutTemplate, BookPage, VoiceTemplate
@@ -12,7 +12,7 @@ from mlnbook_backend.pic_book.serializers import PicBookSerializer, KnowledgePoi
     ChapterSerializer, LayoutTemplateSerializer, ParagraphSerializer, BookSeriesListSerializer, \
     BookSeriesCreateSerializer, BookPageSerializer, BookPageParagraphSerializer, ChapterParagraphSerializer, \
     ChapterPageSerializer, PicBookEditSerializer, VoiceTemplateSerializer, ParagraphBulkSerializer, \
-    ChapterMenuSerializer, ChapterPageMenuSerializer
+    ChapterMenuSerializer, ChapterPageMenuSerializer, IllustrationFileSerializer
 
 from mlnbook_backend.users.models import Author
 from mlnbook_backend.users.serializers import AuthorSerializer
@@ -79,22 +79,30 @@ class PicBookViewSet(viewsets.ModelViewSet):
             chapter_list.append(parent_data)
         return Response(chapter_list)
 
-    @action(detail=True)
-    def chapter_page_menu(self, request, pk=None):
-        pic_book = self.get_object()
-        root_chapter_queryset = Chapter.objects.filter(pic_book=pic_book, parent__isnull=True)
+    def gen_chapter_page_menu(self, root_chapter_queryset):
         chapter_list = []
         for chapter_obj in root_chapter_queryset:
-            # 一层子级目录
-            parent_data = ChapterPageMenuSerializer(chapter_obj).data
-            parent_data["children"] = parent_data.pop("bookpage_set")
+            # 当前仅支持一层子级目录
+            chapter_page_data = ChapterPageMenuSerializer(chapter_obj).data
+            chapter_page_data["children"] = chapter_page_data.pop("bookpage_set")
             children_chapter_queryset = chapter_obj.children.all()
+            # print(chapter_obj.id, chapter_page_data["children"])
             if children_chapter_queryset.exists():
                 children_data = ChapterPageMenuSerializer(children_chapter_queryset, many=True).data
                 for item in children_data:
                     item["children"] = item.pop("bookpage_set")
-                parent_data["children"] = children_data
-            chapter_list.append(parent_data)
+                chapter_page_data["children"] += children_data
+                # print(chapter_obj.id, children_data)
+            sorted_data = sorted(chapter_page_data["children"], key=lambda x: x['seq'])
+            chapter_page_data["children"] = sorted_data
+            chapter_list.append(chapter_page_data)
+        return chapter_list
+
+    @action(detail=True)
+    def chapter_page_menu(self, request, pk=None):
+        pic_book = self.get_object()
+        root_chapter_queryset = Chapter.objects.filter(pic_book=pic_book, parent__isnull=True)
+        chapter_list = self.gen_chapter_page_menu(root_chapter_queryset)
         return Response(chapter_list)
 
     @action(detail=True)
@@ -112,36 +120,30 @@ class PicBookViewSet(viewsets.ModelViewSet):
             chapter_list.append(parent_data)
         return Response(chapter_list)
 
-    @staticmethod
-    def update_menu_attr(cur_key, attr_value, attr="sort_seq"):
-        if "leaf" in str(cur_key):
-            obj_id = int(cur_key.split("_")[1])
-            obj = BookPage.objects.get(id=obj_id)
-            if attr == "sort_seq":
-                setattr(obj, "seq", attr_value)
-            else:
-                setattr(obj, "chapter_id", attr_value)
-            obj.save()
+    @action(detail=True)
+    def page_nums(self, request, pk=None):
+        pic_book = self.get_object()
+        chapter_queryset = Chapter.objects.filter(pic_book=pic_book)
+        has_parent_chapter = chapter_queryset.exclude(parent__isnull=True)
+        page_num_dict = {}
+        if not has_parent_chapter.exists():
+            # 没有子章嵌套直接排序
+            page_id_list = list(BookPage.objects.filter(pic_book=pic_book).values_list("id", flat=True))
+            for num, val in enumerate(page_id_list, start=1):
+                page_num_dict[val] = num
         else:
-            obj = Chapter.objects.get(id=int(cur_key))
-            if attr == "sort_seq":
-                setattr(obj, "seq", attr_value)
-            else:
-                setattr(obj, "parent_id", attr_value)
-            obj.save()
-
-    @action(detail=False, methods=['post'])
-    def sort_menu(self, request, pk=None):
-        # 上传 node 的当前父节点； node需要知道是否是 isLeaf 叶子节点；更新父节点
-        sort_key = request.data["sort_key"]
-        target_parent = request.data["target_parent"]
-        self.update_menu_attr(sort_key, target_parent, "parent")
-        # 上传当前父节点的 子节点，包括子目录和叶子节点；对同一个parent的节点更新排序
-        target_children = request.data["target_children"]
-        for ind in range(len(target_children)):
-            cur_key = target_children[ind]
-            self.update_menu_attr(cur_key, ind)
-        return Response({"detail": "success"})
+            # 存在子章嵌套
+            parent_chapter_queryset = chapter_queryset.filter(parent__isnull=True)
+            chapter_list = self.gen_chapter_page_menu(parent_chapter_queryset)
+            page_id_list = []
+            for page_child_list in chapter_list:
+                for item in page_child_list:
+                    if "isLeaf" in item:
+                        page_id_list.append(item)
+                    else:
+                        page_id_list.extend(item["children"])
+            page_num_dict = {each["id"]: each["seq"] for each in page_id_list}
+        return Response(page_num_dict)
 
 
 class KnowledgePointViewSet(viewsets.ModelViewSet):
@@ -188,11 +190,11 @@ class BookPageViewSet(viewsets.ModelViewSet):
     queryset = BookPage.objects.all()
     serializer_class = BookPageSerializer
 
-    def get_serializer_class(self):
-        if self.action in ["create", "list", "retrieve"]:
-            return BookPageParagraphSerializer
-        else:
-            return BookPageSerializer
+    # def get_serializer_class(self):
+    #     if self.action in ["create", "list", "retrieve"]:
+    #         return BookPageParagraphSerializer
+    #     else:
+    #         return BookPageSerializer
 
     @action(detail=True)
     def paragraph(self, request, pk=None):
@@ -212,7 +214,6 @@ class BookPageViewSet(viewsets.ModelViewSet):
 class ParagraphViewSet(viewsets.ModelViewSet):
     queryset = Paragraph.objects.all()
     serializer_class = ParagraphSerializer
-    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['pic_book', 'chapter']
 
     @action(detail=False, methods=["post"])
@@ -226,7 +227,7 @@ class ParagraphViewSet(viewsets.ModelViewSet):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=['post'])
-    def set_seq(self):
+    def set_seq(self, request, *args, **kwargs):
         seq_list = self.request.data["seq_list"]
         queryset = gen_seq_queryset(seq_list, Paragraph)
         Paragraph.objects.bulk_update(queryset, ["seq"])
@@ -244,13 +245,19 @@ class BookSeriesViewSet(viewsets.ModelViewSet):
             return BookSeriesCreateSerializer
 
 
-class IllustrationFileUploadView(APIView):
-    parser_classes = [FileUploadParser]
+# class IllustrationFileUploadView(APIView):
+#     parser_classes = [FileUploadParser]
+#     permission_classes = [IsAuthenticated]
+#
+#     def post(self, request, filename, format=None):
+#         pic_file = request.data['file']
+#         IllustrationFile(pic_file=pic_file, user=request.user)
+#         return Response({"detail": "success"})
 
-    def put(self, request, filename, format=None):
-        pic_file = request.data['file']
-        IllustrationFile(pic_file=pic_file, user=request.user)
-        return Response(status=204)
+class IllustrationFileUploadView(viewsets.ModelViewSet):
+    # parser_classes = [FileUploadParser]
+    serializer_class = IllustrationFileSerializer
+    queryset = IllustrationFile.objects.all()
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
