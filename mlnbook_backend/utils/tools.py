51c9a8491e086e193a13fbd158e15a79e4225e4d
Devin Mah <lingnck@163.com>
2024-01-15 19:30:25 +0800

    Update tools.py

--- mlnbook_backend/utils/tools.py
+++ mlnbook_backend/utils/tools.py
@@ -10,3 +10,35 @@ def gen_seq_queryset(id_seq_list, model):
     for item in queryset:
         item.seq = id_seq_cfg[item.id]
     return queryset
+
+
+def gen_para_ssml(para_content, para_ssml, voice_name):
+    if not para_ssml:
+        para_ssml_string = """
+        <speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">
+            <voice name="{{voice_name}}">
+                <mstts:express-as  style="angry" >
+                    <prosody rate="0%" pitch="0%">
+                    {{para_content}}
+                    </prosody>
+                </mstts:express-as>
+            </voice>
+        </speak>
+        """.format(para_content=para_content, voice_name=voice_name)
+        return para_ssml_string
+    elif para_ssml.startswith("<voice"):
+        para_ssml_string = """
+        <speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">
+            {{para_ssml}}
+        </speak>
+        """.format(para_ssml=para_ssml)
+        return para_ssml_string
+    else:
+        para_ssml_string = """
+        <speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">
+            <voice name="{{voice_name}}">
+                {{para_ssml}}
+            </voice>
+        </speak>
+        """.format(para_ssml=para_ssml, voice_name=voice_name)
+        return para_ssml_string
