e2836ef298d47428b8d891ae9ffe78e52731c1ad
Devin Mah <lingnck@163.com>
2024-01-15 16:06:06 +0800

    Update models.py

--- mlnbook_backend/tts_labs/models.py
+++ mlnbook_backend/tts_labs/models.py
@@ -3,6 +3,7 @@ import hashlib
 from django.db import models
 from django.conf import settings
 
+from mlnbook_backend.utils.global_choices import AZURE_VOICE_STYLE
 
 REQUEST_CONTENT_CHOICES = (
     ('txt', 'txt'),
@@ -15,15 +16,12 @@ class TTSJobInstance(models.Model):
     language = models.CharField("语言", max_length=16, default="en_US", help_text="language-ios_code, coqui保存路径")
     tts_model = models.CharField("tts模型", max_length=20, default="azure-api",
                                  help_text="开源TTS算法 coqui-ai, 微软语音服务 azure-api")
-    speaker = models.CharField("语音编码", max_length=50, blank=True, null=True)
-    model_name = models.CharField("模型名称", max_length=20, blank=True, null=True, help_text="xtts_v1")
-    dataset = models.CharField("模型数据集", max_length=20, blank=True, null=True, help_text="ljspeech")
-    vocoder = models.CharField("vocoder", max_length=20, blank=True, null=True, help_text="hifigan_v2")
-    extra_params = models.JSONField("语音参数", blank=True, null=True)
-    req_txt = models.TextField("请求文本", help_text="ssml或纯文本")
-    req_type = models.CharField("请求文本类型", max_length=16, default='txt', choices=REQUEST_CONTENT_CHOICES,
-                                help_text="ssml或纯文本")
-    content_uniq = models.CharField("内容标识", max_length=64, help_text="请求内容文本MD5加密，例行时段落标识写入")
+    voice_name = models.CharField("主语音编码", max_length=100, blank=True, null=True)
+    style = models.CharField("说话风格", blank=True, null=True, choices=AZURE_VOICE_STYLE)
+    pitch = models.SmallIntegerField("音调", default=0, help_text="最大+50%，最小-50%")
+    rate = models.SmallIntegerField("语速", default=0, help_text="最大+200%，最小-100%")
+    # volume = models.SmallIntegerField("音量", default=100, help_text="最大100，最小0")
+    req_ssml = models.TextField("请求文本", help_text="ssml或纯文本")
     job_state = models.SmallIntegerField("任务状态", default=0)
     job_detail = models.TextField("任务执行信息", blank=True)
     voice_file = models.FileField("语音文件", upload_to="pic_book/voice_file", null=True, blank=True)
