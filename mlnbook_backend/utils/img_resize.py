c70bb68d0032e6e1f6887732e06d29228ece0067
Devin Mah <lingnck@163.com>
2024-02-02 18:24:20 +0800

    Create img_resize.py

---
+++ mlnbook_backend/utils/img_resize.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import os
+from django.core.files.base import ContentFile
+from pathlib import Path
+from PIL import Image
+from io import BytesIO
+
+image_types = {
+    "jpg": "JPEG",
+    "jpeg": "JPEG",
+    "png": "PNG",
+    "gif": "GIF",
+    "tif": "TIFF",
+    "tiff": "TIFF",
+}
+
+
+def image_resize(image, width=80, height=80):
+    # Open the image using Pillow
+    img = Image.open(image)
+    # check if either the width or height is greater than the max
+    if img.width > width or img.height > height:
+        output_size = (width, height)
+        # Create a new resized “thumbnail” version of the image with Pillow
+        img.thumbnail(output_size)
+        # Find the file name of the image
+        img_filename = Path("small_" + image.name).name
+        # Spilt the filename on “.” to get the file extension only
+        img_suffix = Path(image.name).name.split(".")[-1]
+        # Use the file extension to determine the file type from the image_types dictionary
+        img_format = image_types[img_suffix]
+        # Save the resized image into the buffer, noting the correct file type
+        buffer = BytesIO()
+        img.save(buffer, format=img_format)
+        # Wrap the buffer in File object
+        file_object = ContentFile(img.read(), name=img_filename)
+        # Save the new resized file as usual, which will save to S3 using django-storages
+        return file_object
